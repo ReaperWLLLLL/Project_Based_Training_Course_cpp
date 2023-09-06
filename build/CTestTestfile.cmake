1b4011a0c4e288c7e6ce0fc9e35deeb88563dcd1
王磊 <lei-wang22@mails.tsinghua.edu.cn>
2023-09-06 14:29:37 +0800

    删去多余的目录

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/study/2023SU/c++/code
-# Build directory: G:/study/2023SU/c++/code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
