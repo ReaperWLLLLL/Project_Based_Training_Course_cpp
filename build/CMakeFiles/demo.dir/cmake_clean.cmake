9603cedda45988e59c005ad089ef19288619c345
王磊 <lei-wang22@mails.tsinghua.edu.cn>
2023-09-06 14:28:31 +0800

    clion项目配置

---
+++ build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/main.cpp.obj"
+  "CMakeFiles/demo.dir/main.cpp.obj.d"
+  "demo.exe"
+  "demo.exe.manifest"
+  "demo.pdb"
+  "libdemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
